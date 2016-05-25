c5cc2ffbff518b8e432c5a4faa2cce3a2883cd66
Melvin Carvalho <melvincarvalho@gmail.com>
2016-05-25 12:26:16 +0200

    Add functions

--- bin/create.js
+++ bin/create.js
@@ -11,6 +11,14 @@ var pubKey = util.mnemonicToPubKey(m)
 
 console.log(pubKey)
 
+var privKey = util.mnemonicToPrivKey(m)
+
+console.log(privKey)
+
 address = util.webidAndPubKeyToAddress(webid, 'xpub661MyMwAqRbcGv9ZE8Kc2gG5nJAqHRcUQr5hKimSAqrhQqkjskXots3c49vcUXHGrX7n4pCYbLo9qF2RHFyXQBvHT8zjiEQJqVMeuFrP8Jf')
+
+console.log(address)
+
+address = util.webidAndPrivKeyToAddress(webid, 'xprv9s21ZrQH143K4S5686nbfYKMEGLLsxtd3dA6XLMpcWKiY3RbLDDZM4j8CveaDKv3TtkZpgHFWsGhgN3DmvuZhoLP5V54Fj3Vo8dQAMeuiWS')
 
 console.log(address)

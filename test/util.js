c7511d876f6d7fd86a4053952af2e14425ba1fef
Melvin Carvalho <melvincarvalho@gmail.com>
2016-05-26 13:26:38 +0200

    Test hashToInts

--- test/util.js
+++ test/util.js
@@ -24,6 +24,61 @@ describe('Util Functions', function () {
       expect(( util.hashToInts )).to.be.a('function');
     });
 
+    it('test hashToInts(\'00000000000000000000000000000000\') = \'[0,0,0,0]\'', function () {
+      expect(( util.hashToInts('00000000000000000000000000000000') )).to.eql([0,0,0,0]);
+    });
+
+    it('test hashToInts(\'00000001000000000000000000000000\') = \'[1,0,0,0]\'', function () {
+      expect(( util.hashToInts('00000001000000000000000000000000') )).to.eql([1,0,0,0]);
+    });
+
+    it('test hashToInts(\'7fffffff000000000000000000000000\') = \'[0x7fffffff,0,0,0]\'', function () {
+      expect(( util.hashToInts('7fffffff000000000000000000000000') )).to.eql([0x7fffffff,0,0,0]);
+    });
+
+    it('test hashToInts(\'ffffffff000000000000000000000000\') = \'[0x7fffffff,0,0,0]\'', function () {
+      expect(( util.hashToInts('ffffffff000000000000000000000000') )).to.eql([0x7fffffff,0,0,0]);
+    });
+
+
+    it('test hashToInts(\'00000000000000010000000000000000\') = \'[0,1,0,0]\'', function () {
+      expect(( util.hashToInts('00000000000000010000000000000000') )).to.eql([0,1,0,0]);
+    });
+
+    it('test hashToInts(\'000000007fffffff0000000000000000\') = \'[0,0x7fffffff,0,0]\'', function () {
+      expect(( util.hashToInts('000000007fffffff0000000000000000') )).to.eql([0,0x7fffffff,0,0]);
+    });
+
+    it('test hashToInts(\'00000000ffffffff0000000000000000\') = \'[0,0x7fffffff,0,0]\'', function () {
+      expect(( util.hashToInts('00000000ffffffff0000000000000000') )).to.eql([0,0x7fffffff,0,0]);
+    });
+
+    it('test hashToInts(\'00000000000000000000000100000000\') = \'[0,0,1,0]\'', function () {
+      expect(( util.hashToInts('00000000000000000000000100000000') )).to.eql([0,0,1,0]);
+    });
+
+    it('test hashToInts(\'00000000000000007fffffff00000000\') = \'[0,0,0x7fffffff,0]\'', function () {
+      expect(( util.hashToInts('00000000000000007fffffff00000000') )).to.eql([0,0,0x7fffffff,0]);
+    });
+
+    it('test hashToInts(\'0000000000000000ffffffff00000000\') = \'[0,0,0x7fffffff,0]\'', function () {
+      expect(( util.hashToInts('0000000000000000ffffffff00000000') )).to.eql([0,0,0x7fffffff,0]);
+    });
+
+    it('test hashToInts(\'00000000000000000000000000000001\') = \'[0,0,0,1]\'', function () {
+      expect(( util.hashToInts('00000000000000000000000000000001') )).to.eql([0,0,0,1]);
+    });
+
+    it('test hashToInts(\'0000000000000000000000007fffffff\') = \'[0,0,0,0x7fffffff]\'', function () {
+      expect(( util.hashToInts('0000000000000000000000007fffffff') )).to.eql([0,0,0,0x7fffffff]);
+    });
+
+    it('test hashToInts(\'000000000000000000000000ffffffff\') = \'[0,0,0,0x7fffffff]\'', function () {
+      expect(( util.hashToInts('000000000000000000000000ffffffff') )).to.eql([0,0,0,0x7fffffff]);
+    });
+
+
+
   });
 
 
